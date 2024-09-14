07423070076481cdf407e2da9f0128e285c89c55
mitsui <171986182+mmIPA@users.noreply.github.com>
2024-09-14 00:32:38 +0000

    BootstrapとFontAwesomeの導入

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,14 @@ import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 
+import "jquery";
+import "popper.js";
+import "bootstrap";
+import "../stylesheets/application"; 
+
+import "../stylesheets/homes.scss";
+
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  get 'posts/new'
+  get 'posts/index'
+  get 'posts/show'
+  get 'posts/edit'
   get "/" => "homes#top"
 end
